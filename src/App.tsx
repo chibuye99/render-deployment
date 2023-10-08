2e81c319e6f645b23f98beee4a6f94238f33d727
Chibuye Kunda <chibuyekunda1@gmail.com>
2023-10-08 21:59:53 +0200

    Exercise 1.6-1.14

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,44 @@
-const Header=(props)=>{
-  console.log(props)
-  return(
-     <div>
-      <h1>{props.course.name}</h1>
-     </div>
-  )
+import {useState} from 'react'
 
+const App = ()=>{
+  const [good,setGood] = useState(0)
+  const [neutral,setNeutral] = useState(0)
+  const [bad,setBad]  = useState(0)
+  const [all,setAll] = useState(0)
+  const [average,setAverage] = useState(0)
+  const [postive,setPercentage] = useState(0)
+  
+const handleClickGood = () =>{
+  setGood(good+1)
+  setAll(good+neutral+bad)
+  setAverage((good+neutral+bad)/3)
+  setPercentage((good/all)*100)
+} 
+const handleClickNeutral = () =>{ 
+setNeutral(neutral+1)
+setAll(good+neutral+bad)
+setAverage((good+neutral+bad)/3)
 }
-const Content=(props)=>{
-  console.log(props)
-  return(
-    <div>
-      <p>
-        {props.course.parts[0].name} {props.course.parts[0].exercises}
-      </p>
-      <p>
-        {props.course.parts[1].name} {props.course.parts[1].exercises}
-      </p>
-      <p>
-        {props.course.parts[2].name} {props.course.parts[2].exercises}
-      </p>
-    </div>
-  )
+const handleClickBad = () =>{
+   setBad(bad+1)
+   setAll(good+neutral+bad)
+   setAverage((good+neutral+bad)/3)
+}
 
-}
-const Total=(props)=>{
-  console.log(props)
-  return(
-    <div>
-     <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
-    </div>
-  )
-
-}
-const App = () => {
-  const course ={
-  name:'Half Stack application development',
-  parts: [
-    {
-    name: 'Fundamentals of React',
-    exercises: 10
-    },
-    {
-    name: 'Using props to pass data',
-    exercises: 7
-    },
-    {
-    name: 'State of a component',
-    exercises: 14
-    }
-  ]
-}
-  return (
-    <div>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
-    </div>
-  )
+return(
+  <div>
+    <h1>Give Feedback</h1>
+    <button onClick={handleClickGood}>good</button>
+    <button onClick={handleClickNeutral}>neutral</button>
+    <button onClick={handleClickBad}>bad</button>
+    <h1>Statistics</h1>
+    <p>good: {good}</p>
+    <p>neutral: {neutral}</p>
+    <p>bad: {bad}</p>
+    <p>All:{all} </p>
+    <p>Average: {average} </p>
+    <p>Postive {postive} </p>
+  </div>
+)
 }
 export default App
-
